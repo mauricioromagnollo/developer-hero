6d0337b9cfb0410cae813e2ecaa1a3f672fa8ca6
x0n4d0 <romagnolimauricio@gmail.com>
2022-04-24 01:28:53 -0300

    chore: set initial files

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+import { Env } from '@/main/config';
+import { AppModule } from '@/app.module';
 
 async function bootstrap() {
+  new Env();
+  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: Env.CORS_ORIGIN,
+    credentials: true,
+  });
+
+  await app.listen(Env.PORT);
+
+  logger.log(`[*] Server running at ${Env.URL}:${Env.PORT}`);
 }
+
 bootstrap();
